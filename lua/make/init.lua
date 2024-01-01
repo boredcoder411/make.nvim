220e5171cedcfcb15d8c78536e270cd7c4c289e8
boredcoder411 <105937655+boredcoder411@users.noreply.github.com>
2024-01-01 11:46:38 +0100

    Update init.lua

--- lua/make/init.lua
+++ lua/make/init.lua
@@ -4,7 +4,7 @@ local config = {}
 
 function M.setup()
 	config = vim.tbl_deep_extend("force", config, setup_config or {})
-	vim.api.nvim_create_user_command("MakePanel", require("make").make(), {})
+	vim.api.nvim_create_user_command("MakePanel", M.make(), {})
 end
 
 function M.make()
